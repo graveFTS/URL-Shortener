b382628cf7a433986e8e11b80aee6bb282379a00
Dipsekhar Maity <dipsekharmaity9723@gmail.com>
2024-08-15 21:58:30 +0530

    file update

--- index.js
+++ index.js
@@ -1,8 +1,34 @@
-const express = require('express');
+const express = require("express");
+
+const { connectToMongoDB } = require("./connect");
+const urlRoute = require("./routes/url");
+const URL = require("./models/url");
 
 const app = express();
 const PORT = 3000;
 
+connectToMongoDB("mongodb://localhost:27017/url-shortenerDB").then(() =>
+  console.log("Connected to MongoDB")
+);
 
+app.use(express.json());
 
-app.listen(PORT, () => console.log('Server started at PORT ' + PORT));
+app.use("/url", urlRoute);
+app.get("/:shortId", async (req, res) => {
+  const shortId = req.params.shortId;
+  const entry = await URL.findOneAndUpdate(
+    {
+      shortId,
+    },
+    {
+      $push: {
+        visitHistory: {
+          timestamp: Date.now(),
+        },
+      },
+    }
+  );
+  res.redirect(entry.redirectURL);
+});
+
+app.listen(PORT, () => console.log("Server started at PORT " + PORT));
